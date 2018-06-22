0c486e32cb1a84d27086ea5d7a31c2a42a20070e
Gabriel Hutchison <gabehwebsites@gmail.com>
2018-06-21 21:52:53 -0400

    Improve readability for Login tests

--- src/Login.test.js
+++ src/Login.test.js
@@ -18,25 +18,25 @@ it('contains the app\'s name', () => {
 });
 
 it('contains a form', () => {
-  expect(testElem.getElementsByTagName('form')).not.toHaveLength(0);
+  expect(testElem.querySelector('form')).toBeTruthy();
 });
 
 it('requests user email', () => {
-  expect(testElem.querySelectorAll('input[type=email]')).not.toHaveLength(0);
+  expect(testElem.querySelector('input[type=email]')).toBeTruthy();
 });
 
 it('requests user password', () => {
-  expect(testElem.querySelectorAll('input[type=password]')).not.toHaveLength(0);
+  expect(testElem.querySelector('input[type=password]')).toBeTruthy();
 });
 
 it('has login button', () => {
-  expect(testElem.querySelectorAll('input[type=submit]')).not.toHaveLength(0);
+  expect(testElem.querySelector('input[type=submit]')).toBeTruthy();
 });
 
 it('has forgot link', () => {
-  expect(testElem.querySelectorAll('a.forgot')).not.toHaveLength(0);
+  expect(testElem.querySelector('a.forgot')).toBeTruthy();
 });
 
 it('has create new account button', () => {
-  expect(testElem.querySelectorAll('input[type=button].register')).not.toHaveLength(0);
+  expect(testElem.querySelector('input[type=button].register')).toBeTruthy();
 });
