75385fe16d7500b5713819880d69d7875bd25310
Gabriel Hutchison <gabehwebsites@gmail.com>
2018-06-30 09:23:17 -0400

    Revert "Implement register form validation/submission"
    This reverts commit 42f72cf2698ca40910a4bd8eb3c758b37ba70652.

--- src/Register.js
+++ src/Register.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import config from './config.json';
-import ReactLoading from 'react-loading';
-import './Register.css';
-import { submit } from './register-form.js';
 
 export default class Register extends Component {
   render() {
     return (
       <section className="register">
         <h3>Register to {config.APP_NAME}</h3>
-        <form method="POST" onSubmit={submit}>
+        <form method="POST" action="/api/users/register">
           <input name="email" type="email" placeholder="Email" />
           <br />
-          <input name="username" placeholder="Username" />
+          <input name="first" placeholder="First" />
+          <input name="last" placeholder="Last" />
           <br />
           <input name="password" type="password" />
           <br />
           <input name="passwordconfirm" type="password" />
           <br />
           <input type="submit" value="Register" />
-          <br />
-          <span className="spinner hidden">
-            <ReactLoading type="spokes" color="#000" />
-          </span>
         </form>
       </section>
     );
